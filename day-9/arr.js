43b309f7da73eee427b9ebe4e005eadbbc829441
skg21 <skg21797@gmail.com>
2020-11-01 21:18:15 +0530

    javascript file

--- day-9/arr.js
+++ day-9/arr.js
@@ -6,6 +6,35 @@ for (var i = 0; i < 10; i++) {
 
 arr = [0, 1, 4, 9, 16, 25, 36, 49, 68, 81];
 
-for (var i = 0; 1 < 10; i++) {
-    arr[1] = i * i;
+for (var i = 0; i < 10; i++) {
+    arr[i] = i * i;
 }
+
+console.log(i);
+
+//-----function-----//
+function sum() {
+    var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    var sum = 0;
+    for (var i = 0; i < 10; i++) {
+        sum += arr[i];
+    }
+    console.log(sum);
+}
+sum();
+//---------//
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+var a = 0;
+var a = 1;
+var a = 2;
+var a = 3;
+
+for (var a = 0; a < 10; a++) {
+    arr[a] = a + 1;
+}
+console.log(a)
+
+
+//-----------//
+
+
