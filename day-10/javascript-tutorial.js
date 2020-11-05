23b3b98216e252bcbdaffd4c855b65fc928ff0ae
skg21 <skg21797@gmail.com>
2020-11-05 21:24:03 +0530

    adding test javascript file

---
+++ day-10/javascript-tutorial.js
@@ -0,0 +1,9 @@
+var arr = [[10, 20], [100, 200], [1000, 2000]];
+sum = 0;
+for (var i = 0; i < arr.length; i++) {
+    sum += arr[i];
+    for (var j = 0; j < arr2.length; j++); {
+        sum += arr2[j];
+    }
+}
+console.log(sum);
