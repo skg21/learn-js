8d3fb0e7fc8d050cc11569caf2e770f33144a4d4
skg21 <skg21797@gmail.com>
2020-12-09 21:28:45 +0530

    domManupulate.js

--- day-21/index.js
+++ day-21/index.js
@@ -16,12 +16,52 @@ function getTeacherDetails() {
             villageDOM.innerHTML += teacher.village;
 
             var ageDOM = document.getElementById("teacher-age");
-            ageDOM.innerHTML += teacher.age;
 
             var subject = document.getElementById("teacher-subject");
+            ageDOM.innerHTML += teacher.age; ");
             subject.innerHTML += teacher.subject;
         }
     }
 }
+//---------------------------
+function getStudentsDetails() {
+    var url = "http://127.0.0.1:3000/";
+    var req = new XMLHttpRequest();
+    req.open("GET", url);
+    req.send();
+    req.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            var students = JSON.parse(this.responseText);
+            console.log(students.name);
+            console.log(students.age);
 
+            var nameDOM = document.getElementById("students-name");
+            nameDOM.innerHTML += students.name;
+
+            var villageDom = document.getElementById("students-villege");
+            villageDom.innerHTML += students.village;
+
+            var ageDom = document.getElementById("students-age");
+            ageDom.innerHTML += students.age;
+
+            var subjectDOM = document.getElementById("students-subject");
+            subjectDOM.innerHTML += students.subject;
+        }
+    }
+}
+function userDatadetails() {
+    var url = "";
+    var req = new XMLDocument();
+    req.open("GET", url);
+    req.send();
+    req.onreadystatechange = function () {
+        if (this.onreadystatechange === 4) {
+            var students = JSON.parse(this.responseText);
+            console.log(students.name);
+            console.log(students.age);
+
+            var namrDom = document.getElementById("students-name");
+        }
+    }
+}
 
