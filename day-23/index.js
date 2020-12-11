b5e8c9aad4d6ae7ebcebfcf8899c36dae5eb4056
skg21 <skg21797@gmail.com>
2020-12-11 21:19:33 +0530

    usermanagement.js

---
+++ day-23/index.js
@@ -0,0 +1,11 @@
+function createUser() {
+    var fullName = document.getElementById("fullname").value;
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+    var DoB = document.getElementById("dob").value;
+    var Gender = document.getElementById("male-gender").value;
+    var Gender = document.getElementById("female-gender").value;
+    var country = document.getElementById("country").value;
+    alert(country);
+
+} 
