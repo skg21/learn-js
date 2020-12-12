0285384d5a8499ebdb014cb05eb74c4e99f11fd5
skg21 <skg21797@gmail.com>
2020-12-12 19:39:57 +0530

    adding file

--- day-23/index.js
+++ day-23/index.js
@@ -2,10 +2,33 @@ function createUser() {
     var fullName = document.getElementById("fullname").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var DoB = document.getElementById("dob").value;
-    var Gender = document.getElementById("male-gender").value;
-    var Gender = document.getElementById("female-gender").value;
-    var country = document.getElementById("country").value;
-    alert(country);
+    var dob = document.getElementById("dob").value;
 
-} 
+    var maleGender = document.getElementById("male-gender");
+    var femaleGender = document.getElementById("female-gender");
+
+    var gender = "";
+
+    if (maleGender.checked) {
+        gender = maleGender.value;
+    }
+
+    if (femaleGender.checked) {
+        gender = femaleGender.value;
+    }
+
+    var country = document.getElementById("country").value;
+
+    var u = new User(fullName, email, password, dob, gender, country);
+
+    console.log(u);
+}
+
+function User(fullName, email, password, dob, gender, country) {
+    this.fullName = fullName,
+        this.email = email,
+        this.password = password,
+        this.dob = dob,
+        this.gender = gender,
+        this.country = country
+}
