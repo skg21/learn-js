afb74a41c015c87734cf02e6185681120df8b4cc
skg21 <skg21797@gmail.com>
2021-01-09 20:19:51 +0530

    adding .js file

--- day-25/multidimance.js
+++ day-25/multidimance.js
@@ -19,3 +19,22 @@ for (var i = 0; i < arr.length; i++) {
         console.log(arr[i][j]);
     }
 }
+
+var arr2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10][11, 12, 13, 14, 15]];
+
+for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr[i].length; j++) {
+        console.log(arr2[i][j]);
+    }
+}
+
+var arr3 = [[7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]];
+for (var i = 0; i < arr3.length; i++) {
+    for (var j = 0; j < arr3[i].length; j++) {
+        console.log(arr3[i][j]);
+    }
+}
+
+//-----------------------------------//
+
+//------function =exewte multipte statemeuts,----//
