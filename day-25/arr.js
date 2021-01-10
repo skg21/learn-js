e33993b3c5c4030fedc84f2f717fd3e0b4b31ef5
skg21 <skg21797@gmail.com>
2021-01-10 21:19:45 +0530

    adding file

--- day-25/arr.js
+++ day-25/arr.js
@@ -1,14 +1,7 @@
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-array.push(11);
-array.push(12, 13, 14, 15);
-//------------------------------//
-//--pop--//
-
+array.push(11, 12, 13, 14, 15);
 array.pop();
-
-//-------------------------//
-//--shift--//
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 array.shift();
-array.unshift(10);
+array.unshift(1);
+console.log(array);
