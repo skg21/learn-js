2d922147c514e30af5fd7ade55ffebed30072516
skg21 <skg21797@gmail.com>
2021-01-12 20:56:07 +0530

    adding.js file

--- array/printArray.js
+++ array/printArray.js
@@ -1,12 +1,28 @@
-var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+/*var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
-for(var i = 0; i < arr1.length; i++) {
+for (var i = 0; i < arr1.length; i++) {
 	console.log(arr1[i]);
 }
 
 console.log("print array in reverse");
 
-for(var j = arr1.length-1; j >= 0; j--) {
+for (var j = arr1.length - 1; j >= 0; j--) {
 	console.log(arr1[j]);
+}*/
+
+var arr = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for (var i = arr.length - 1; i >= 0; i--) {
+	console.log(arr[i]);
+
+}
+var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = arr1.length - 1; i >= 5; i--) {
+	console.log(arr1[i]);
+}
+
+
+var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < arr.length / 2; i++) {
+	console.log(arr2[i]);
 }
--- array/printArray2.js
+++ array/printArray2.js
@@ -1,18 +1,32 @@
-var arr1 = [[1, 2], [3, 4], [5, 6], [7,8], [9, 10]];
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i > arr.length; i++) {
+	console.log(arr[i]);
+}
+/*
+var arr1 = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]];
 
 // print array as 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-for(var i = 0; i < arr1.length; i++) {
-	console.log(arr1[i][0]);
+for (var i = 0; i < arr1.length; i++) {
+	console.log(arr1[i][0]); 1
 	console.log(arr1[i][1]);
 }
 
 // print using inner for loop
 //
-for(var i = 0; i <  arr1.length; i++) {
-	for(var j = 0; j < arr1[i].length; j++) {
+for (var i = 0; i < arr1.length; i++) {
+	for (var j = 0; j < arr1[i].length; j++) {
 		console.log(arr1[i][j]);
 	}
 }
 
-//print array as 2,1, 4,3, 6,5, 8,7, 10,9
+//print array as 2,1, 4,3, 6,5, 8,7, 10,9;
+
+var arr2 = [[5, 6,], [7, 8], [9, 10], [10, 11], [12, 13]];
+
+for (var i = 0; i < arr2.length; i++) {
+	for (var j = 0; j < arr2[i].length; j++) {
+		g
+		console.log(arr2[i][j]);
+	}
+}*/
 
