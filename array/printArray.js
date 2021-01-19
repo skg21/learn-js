90a3bd47eb846e518d3d0664abf9a75f0398abdc
skg21 <skg21797@gmail.com>
2021-01-19 21:28:15 +0530

    js tutorial

--- array/printArray.js
+++ array/printArray.js
@@ -1,28 +1,17 @@
-/*var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-
-for (var i = 0; i < arr1.length; i++) {
-	console.log(arr1[i]);
-}
-
-console.log("print array in reverse");
-
-for (var j = arr1.length - 1; j >= 0; j--) {
-	console.log(arr1[j]);
-}*/
-
-var arr = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-for (var i = arr.length - 1; i >= 0; i--) {
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//--arr-printing---//
+/*
+for (var i = 0; i < arr.length; i++) {
 	console.log(arr[i]);
+} */
 
-}
-var arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var i = arr1.length - 1; i >= 5; i--) {
-	console.log(arr1[i]);
+//----arr--reverse--//
+for (var i = arr.length; i >= 0; i--) {
+	console.log(arr[i]);
 }
 
-var arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var i = 0; i < arr.length / 2; i++)
-	console.log(arr2[i]);
+//----arr---hafe-last-reverse--//
+
+for (var i = arr.length / 2; i >= 5; i--) {
+	console.log(arr[i]);
 }
-var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
--- day-25/multidimance.js
+++ day-25/multidimance.js
@@ -1,40 +1,24 @@
-var arr = [[1, 2], [3, 4, 100], [5, 6]];
-
-for (var i = 0; i < arr[0].length; i++) {
-    console.log(arr[0][i]);
+var num = 30;
+function test(num) {
+    if (num > 20) {
+        return "num is bigges then;"
+    }
+    return "num is less then 20";
 }
+var result = test();
+console.log(result);
 
-for (var i = 0; i < arr[1].length; i++) {
-    console.log(arr[1][i]);
+var a = 5;
+var b = 7;
+var c = 8;
+
+if (a > b) {
+    console.log("a");
+} else {
+    console.log("c");
 }
-
-for (var i = 0; i < arr[2].length; i++) {
-    console.log(arr[2][i]);
-}
-
-
-
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        console.log(arr[i][j]);
+{
+    if (b > c) {
+        console.log("c");
     }
 }
-
-var arr2 = [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10][11, 12, 13, 14, 15]];
-
-for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr[i].length; j++) {
-        console.log(arr2[i][j]);
-    }
-}
-
-var arr3 = [[7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20]];
-for (var i = 0; i < arr3.length; i++) {
-    for (var j = 0; j < arr3[i].length; j++) {
-        console.log(arr3[i][j]);
-    }
-}
-
-//-----------------------------------//
-
-//------function =exewte multipte statemeuts,----//
