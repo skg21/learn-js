35ef9b66a4cc4adc3d2f3a25a4fd3f433ada3e59
skg21 <skg21797@gmail.com>
2021-02-02 21:49:00 +0530

    adding js

--- day-28/obj.js
+++ day-28/obj.js
@@ -1,38 +1,16 @@
 var myDog = {
-    "name": "Coder",
-    "legs": 4,
-    "tails": 1,
-    "friends": ["freeCodeCamp Campers", "jhabbu"]
-  };
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers", "jhabbu"]
+};
 
-myDog.tails = 0;
+myDog.tails = 6;
 
 //add new property in object
 
-myDog.bark = "woo wooh";
+myDog.name = "moto";
 
+// delete myDog.name;
 console.log(myDog);
 
-delete myDog.bark;
-
-console.log(myDog);
-
-
-delete myDog.tails;
-console.log(myDog);
-
-//-------------------------------//
-var lookup = {
-    alpha: "Adams",
-    charlie: "chicago",
-    delta: "Denver",
-    echo: "Easy",
-    foxtrot: "Frank"
-  }
-  
-   var obj = lookup;
-   console.log(obj);
-
-
-
- 
