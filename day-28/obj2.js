5dc7aab6b06f066b2562a8411c8ae0d97fac04b7
skg21 <skg21797@gmail.com>
2021-02-09 21:47:42 +0530

    adding js

--- day-28/obj2.js
+++ day-28/obj2.js
@@ -1,35 +1,13 @@
-var Animations = [
-  {
-    name: "Charlie",
-    color: "black and white",
-    age: "3years"
-  },
-  {
-    name: "Loki",
-    color: "gray",
-    age: "2years"
-  },
-  {
-    name: "jack",
-    color: "bron",
-    age: "3years"
-  },
-  {
-    name: "Simba",
-    color: "cream",
-    age: "5years"
+var car = {
+  brand: "suguki",
+  color: "red",
+  gear: 5,
+  tyre: {
+    color: "black",
+    brand: "MRF",
+    width: 6
 
-  },
-  {
-    name: "",
-    color: "Fawn",
-    age: "4years"
   }
-];
-
-console.log(Animations);
-
-Animations[0].name;
-Animations[3].age;
-console.log(Animations[3].age);
-
+};
+car.brand = "mahindira";
+car.tyre.brand = "CEAT";
