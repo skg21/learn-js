98c207699004de9846a27b69f921b57e9619ce17
skg21 <skg21797@gmail.com>
2021-02-10 21:42:18 +0530

    adding js

--- day-28/obj2.js
+++ day-28/obj2.js
@@ -1,13 +1,1 @@
-var car = {
-  brand: "suguki",
-  color: "red",
-  gear: 5,
-  tyre: {
-    color: "black",
-    brand: "MRF",
-    width: 6
 
-  }
-};
-car.brand = "mahindira";
-car.tyre.brand = "CEAT";
