906fb19681d0d5087da5ff0cdf11ea39614e88f4
skg21 <skg21797@gmail.com>
2021-02-22 21:29:29 +0530

    adding js

--- logics/pattern-3.js
+++ logics/pattern-3.js
@@ -20,6 +20,32 @@ for (var j = 1; j <= 5; j += 1) {
     }
     process.stdout.write("\n");
 };
+console.log("-----------------------------------------");
+for (var j = 1; j >= 5; j++) {
+    for (var i = 5; i >= j; i--) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+};
+console.log("========================");
+for (var j = 1; j <= 5; j++) {
+    for (var i = 5; i >= j; i--) {
+        process.stdout.write("*");
+    };
+    process.stdout.write("\n");
+};
+console.log("===========================================");
+console.log("===========================================");
+
+
+for (var j = 5; j >= 0; j--) {
+    for (var i = 1; i <= j; i++) {
+        process.stdout.write("*");
+    }
+    process.stdout.write("\n");
+};
+
+
 
 
 
