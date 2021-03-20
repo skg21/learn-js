c6467660362b7b32acbd13c0352732311c1b68a9
skg21 <skg21797@gmail.com>
2021-03-20 21:28:00 +0530

    adding js

--- ecma-script/basic.js
+++ ecma-script/basic.js
@@ -3,12 +3,9 @@ if (true) {
     console.log("local: ", k);
 }
 console.log("global: ", k);
-function checkScope() {
-    var i = 'function scope';
-    if (true) {
-        i = 'block scope';
-        console.log('Block scope i is: ', i);
-    }
-    console.log('Function scope i is: ', i);
-    return i;
+
+if (true) {
+    let l = 10;
+    console.log("local:", l);
 }
+console.log("global:", l);
